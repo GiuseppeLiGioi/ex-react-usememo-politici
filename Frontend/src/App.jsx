752cd888be05276aa26b5cacb65a950e3efe644c
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-05-26 12:02:18 +0200

    added. milestone 1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -14,14 +14,52 @@ Breve biografia (biography)
 
 Obiettivo: Caricare e mostrare i politici in un’interfaccia chiara e leggibile.
 */
+import { useState, useEffect } from "react"
 
 function App() {
 
+  const [politicians, setPoliticians] = useState([]);
+
+  async function fetchPolitici(){
+  try{
+    const response = await fetch('http://localhost:3333/politicians')  
+     if(!response){
+      throw new Error("Errore nel fetch dei politici")
+     }
+     const politicians = await response.json()
+    setPoliticians(politicians)
+   }catch(error){
+    console.error(error)
+   }
+  
+  }
+
+  useEffect( () => {
+   fetchPolitici();
+  }, [])
+
+
+  
 
   return (
     <>
-     
+    <h1 className="title">LISTA DEI POLITICI</h1>
+    <div className="container_card">
+     {
+      politicians.map((p, index) => (
+        
+          <div  key={index} className="single_card">
+          <h2>{p.name}</h2>
+          <img src={p.image} className="img-card"/> 
+          <h5>{p.position}</h5>
+          <p>{p.biography}</p>
+          </div>
+        
+      ))
+     }
+     </div>
     </>
+    
   )
 }
 
